2c5d851a1cad736e6d0931b15e24bce6313c6ac7
linus-rudbeck <linus.rudbeck@distansakademin.se>
2024-01-16 14:35:36 +0100

    Update Action

--- src/main/java/se/distansakademin/Main.java
+++ src/main/java/se/distansakademin/Main.java
@@ -2,6 +2,9 @@ package se.distansakademin;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello my na
+        System.out.println("Hello my name is Jekyll!");
+        System.out.println("I am a guide in this place!");
+        System.out.println("Some people never make it.");
+        System.out.println("Most people do...");
     }
 }
